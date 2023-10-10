0bd80a0113241db5cda5318c7cfe1a3f93a300e4
codesbyaystyles <143831850+codesbyaystyles@users.noreply.github.com>
2023-10-10 17:31:41 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let s = document.querySelector('.gggh');
+s.addEventListener('click', function(){
+    document.querySelectorAll('.hide').style.display = 'none';
+})
---
+++ index3.js
@@ -0,0 +1,20 @@
+food = {
+    class:['carbo','prot','mine','star'],
+    identity:['yam','bread','rice'],
+}
+console.log(`${food.class[1]}`)
+
+const impure = ()=> {
+    x++;
+}
+   var haveFun = ()=> console.log('potantoes');
+
+   setTimeout( haveFun,2000);
+
+   setTimeout((tt)=>{
+   console.log('bread')   
+   },3000);
+//    setInterval(haveFun,4000)
+setInterval({
+tt()
+},4000)
